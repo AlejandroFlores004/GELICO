8d5c1ac6d8e7c71878244894ee207a89289c2e25
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-06 14:46:14 -0600

    Add models and admin configurations for Distrito, Escuela, CDE, and Encargado

--- escuela/admin.py
+++ escuela/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import Distrito, Escuela, CDE, Encargado
 
-# Register your models here.
+
+@admin.register(Distrito)
+class DistritoAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+
+@admin.register(Escuela)
+class EscuelaAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'nombre_corto', 'distrito', 'estado')
+    list_filter = ('estado', 'distrito')
+    search_fields = ('codigo', 'nombre', 'nombre_corto')
+    ordering = ('codigo',)
+    fieldsets = (
+        ('Información Básica', {
+            'fields': ('codigo', 'nombre', 'nombre_corto')
+        }),
+        ('Relaciones', {
+            'fields': ('distrito',)
+        }),
+        ('Estado', {
+            'fields': ('estado',)
+        }),
+    )
+
+
+@admin.register(CDE)
+class CDEAdmin(admin.ModelAdmin):
+    list_display = ('escuela', 'FechaInicio', 'FechaFin', 'estado')
+    list_filter = ('estado', 'escuela', 'FechaInicio')
+    search_fields = ('escuela__nombre',)
+    ordering = ('-FechaInicio',)
+    fieldsets = (
+        ('Fechas', {
+            'fields': ('FechaInicio', 'FechaFin')
+        }),
+        ('Relaciones', {
+            'fields': ('escuela',)
+        }),
+        ('Estado', {
+            'fields': ('estado',)
+        }),
+    )
+
+
+@admin.register(Encargado)
+class EncargadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'email', 'telefono', 'escuela', 'estado')
+    list_filter = ('estado', 'escuela')
+    search_fields = ('nombre', 'apellido', 'email', 'telefono')
+    ordering = ('apellido', 'nombre')
+    fieldsets = (
+        ('Información Personal', {
+            'fields': ('nombre', 'apellido')
+        }),
+        ('Contacto', {
+            'fields': ('email', 'telefono')
+        }),
+        ('Relaciones', {
+            'fields': ('escuela',)
+        }),
+        ('Estado', {
+            'fields': ('estado',)
+        }),
+    )
--- escuela/models.py
+++ escuela/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+class Distrito(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Distrito"
+        verbose_name_plural = "Distritos"
+
+class Escuela(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=255, unique=True)
+    nombre_corto = models.CharField(max_length=100, unique=True)
+    distrito = models.ForeignKey(Distrito, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre} - {self.distrito.nombre}"
+    
+    class Meta:
+        verbose_name = "Escuela"
+        verbose_name_plural = "Escuelas"
+
+class CDE(models.Model):
+    FechaInicio = models.DateField()
+    FechaFin = models.DateField()
+    estado = models.BooleanField(default=True)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"CDE: {self.escuela.nombre_corto} - del {self.FechaInicio} al {self.FechaFin}"
+    
+    class Meta:
+        verbose_name = "CDE"
+        verbose_name_plural = "CDEs"
+
+class Encargado(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    estado = models.BooleanField(default=True)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} - {self.escuela.nombre_corto}"
+    
+    class Meta:
+        verbose_name = "Encargado"
+        verbose_name_plural = "Encargados"
