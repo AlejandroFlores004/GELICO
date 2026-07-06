36b8a14ffc61007d30c72d1f78be0a08bd3f6d2d
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-06 14:50:46 -0600

    Add 'programacion' app with initial structure and configurations

--- programacion/apps.py
+++ programacion/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CalendarioConfig(AppConfig):
+class ProgramacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calendario'
+    name = 'programacion'
