ef93b782e4ae6469485f01a2128fadc59b9778b5
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-10 14:54:12 -0600

    Add liquidaciones section with URL routing and template

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+def home_liquidaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Liquidaciones', 'url': reverse('home_liquidaciones')},
+    ]
+    return render(
+        request, 
+        'home_liquidaciones.html', 
+        {'breadcrumbs': breadcrumbs}
+    )
