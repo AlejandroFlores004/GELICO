8112e1b064184c98fa07e26543729e90622c43ab
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-16 17:08:41 -0600

    Add SeleccionarEscuelaForm and integrate it into the Liquidaciones page for school selection

---
+++ liquidacion/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django_select2.forms import Select2Widget
+from escuela.models import Escuela
+
+class SeleccionarEscuelaForm(forms.Form):
+    escuela = forms.ModelChoiceField(
+        queryset=Escuela.objects.all(),
+        widget=Select2Widget(attrs={
+            'data-placeholder': 'Seleccione una escuela',
+            'style': 'width: 100%',
+        }),
+        label='Escuela',
+        required=True
+    )
--- liquidacion/views.py
+++ liquidacion/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse
+from . import forms
 
 # Create your views here.
 def home_liquidaciones(request):
+    
+    seleccionarEscuelas = forms.SeleccionarEscuelaForm(request.GET or None)
+
+
     breadcrumbs = [
         {'name': 'Inicio', 'url': reverse('home')},
         {'name': 'Liquidaciones', 'url': reverse('home_liquidaciones')},
     ]
+
     return render(
         request, 
         'home_liquidaciones.html', 
-        {'breadcrumbs': breadcrumbs}
+        {
+            'breadcrumbs': breadcrumbs, 
+            'seleccionarEscuelas': seleccionarEscuelas
+        }
     )
