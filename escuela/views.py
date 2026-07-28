bf5a5ca188784a19385efc4e54ac7f1669f7347f
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-28 09:46:39 -0600

    Add encargadoHome view and template, and update URL routing for escuela

--- escuela/views.py
+++ escuela/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def encargadoHomeView(request):
+    return render(request, 'encargado/encargadoHome.html')
--- gelico/urls.py
+++ gelico/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('liquidaciones/', include('liquidacion.urls')),
+    path('escuela/',include('escuela.urls'))
 ]
 
 if settings.DEBUG:
