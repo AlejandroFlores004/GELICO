b5244bab3ceed13349affd6e9f899b32c28a7fbd
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-31 21:00:17 -0600

    Add liquidacionHome template and refactor views for school and receipt management

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -21,8 +21,8 @@ def home_liquidaciones(request):
     ]
 
     return render(
-        request,
-        'liquidacion/liquidacionHome.html',
+        request, 
+        'liquidacion/liquidacionHome.html', 
         {
             'breadcrumbs': breadcrumbs,
             'seleccionarEscuelas': seleccionarEscuelas,
@@ -137,12 +137,8 @@ def asignacion_eliminar(request, pk):
         _, asignaciones = _filtrar_asignaciones(request)
         return render(
             request,
-            'partials/asignacion/_asignacion_form_success.html',
-            {'asignaciones': asignaciones}
+            'partials/liquidacion/_escuela_info_result.html',
+            {'escuela': escuela, 'encargado': encargado}
         )
 
-    return render(
-        request,
-        'partials/asignacion/_asignacion_delete_modal.html',
-        {'instance': instance}
-    )
+    return render(request, 'partials/liquidacion/_escuela_info.html', {})
