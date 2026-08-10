c3d62ffeb1cea9b3b5cd41731f031f7f189c4a81
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-08-10 16:33:48 -0600

    Refactor forms and templates for dynamic bono selection based on escuela

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -2,8 +2,8 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from . import forms
-from escuela.models import Escuela, Encargado
-from .models import Asignacion
+from escuela.models.models import Escuela, Encargado
+from .Escuela, Encargado import Asignacion
 
 # Create your views here.
 
@@ -28,8 +28,8 @@ def home_liquidaciones(request):
     ]
 
     return render(
-        request, 
-        'liquidacion/liquidacionHome.html', 
+        request,
+        'liquidacion/liquidacionHome.html',
         {
             'breadcrumbs': breadcrumbs,
             'seleccionarEscuelas': seleccionarEscuelas,
@@ -49,9 +49,447 @@ def buscar_escuela(request):
         return render(
             request,
             'partials/liquidacion/_escuela_info_result.html',
-            {'escuela': escuela, 'encargado': encargado}
+            {'escuela': escuela, 'encargado': encargado, 'seleccionarBono': seleccionarBono}
         )
+    return render(request, 'partials/liquidacion/_escuela_info.html', {})
 
+#Asignaciones views
+def _filtrar_asignaciones(request):
+    filtro_form = forms.FiltrarAsignacionesForm(request.GET or None)
+
+    asignaciones_list = Asignacion.objects.select_related(
+        'escuela', 'escuela__distrito', 'bono'
+    ).order_by('-fecha')
+
+    if filtro_form.is_valid():
+        escuela = filtro_form.cleaned_data.get('escuela')
+        bono = filtro_form.cleaned_data.get('bono')
+        fecha = filtro_form.cleaned_data.get('fecha')
+
+
+        if escuela:
+            asignaciones_list = asignaciones_list.filter(escuela=escuela)
+        if bono:
+            asignaciones_list = asignaciones_list.filter(bono=bono)
+        if fecha:
+            asignaciones_list = asignaciones_list.filter(fecha=fecha)
+
+    paginator = Paginator(asignaciones_list, 20)
+    asignaciones = paginator.get_page(request.GET.get('page'))
+
+    return filtro_form, asignaciones
+
+
+def home_asignaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Asignaciones', 'url': reverse('home_asignaciones')},
+    ]
+
+    filtro_form, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'asignacion/asignacionHome.html',
+        {
+            'breadcrumbs': breadcrumbs,
+            'asignaciones': asignaciones,
+            'filtro_form': filtro_form,
+            'form_media': filtro_form.media,
+        }
+    )
+
+def buscar_asignaciones(request):
+    _, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'partials/asignacion/_listado_asignaciones.html',
+        {'asignaciones': asignaciones}
+    )
+>>>>>>> 8859bd7 (Implement filtering functionality for asignaciones with search form and pagination)
+
+#Asignaciones views
+def _filtrar_asignaciones(request):
+    filtro_form = forms.FiltrarAsignacionesForm(request.GET or None)
+
+    asignaciones_list = Asignacion.objects.select_related(
+        'escuela', 'escuela__distrito', 'bono'
+    ).order_by('-fecha')
+
+    if filtro_form.is_valid():
+        distrito = filtro_form.cleaned_data.get('distrito')
+        escuela = filtro_form.cleaned_data.get('escuela')
+        bono = filtro_form.cleaned_data.get('bono')
+        fecha = filtro_form.cleaned_data.get('fecha')
+
+
+        if distrito:
+            asignaciones_list = asignaciones_list.filter(escuela__distrito=distrito)
+        if escuela:
+            asignaciones_list = asignaciones_list.filter(escuela=escuela)
+        if bono:
+            asignaciones_list = asignaciones_list.filter(bono=bono)
+        if fecha:
+            asignaciones_list = asignaciones_list.filter(fecha=fecha)
+
+    paginator = Paginator(asignaciones_list, 20)
+    asignaciones = paginator.get_page(request.GET.get('page'))
+
+    return filtro_form, asignaciones
+
+
+def home_asignaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Asignaciones', 'url': reverse('home_asignaciones')},
+    ]
+
+    filtro_form, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'asignacion/asignacionHome.html',
+        {
+            'breadcrumbs': breadcrumbs,
+            'asignaciones': asignaciones,
+            'filtro_form': filtro_form,
+            'form_media': filtro_form.media,
+        }
+    )
+
+
+def buscar_asignaciones(request):
+    _, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'partials/asignacion/_listado_asignaciones.html',
+        {'asignaciones': asignaciones}
+    )
+
+
+def asignacion_form(request, pk=None):
+    instance = get_object_or_404(Asignacion, pk=pk) if pk else None
+
+    if request.method == 'POST':
+        form = forms.AsignacionForm(request.POST, instance=instance, prefix='asignacion')
+        if form.is_valid():
+            form.save()
+            _, asignaciones = _filtrar_asignaciones(request)
+            return render(
+                request,
+                'partials/asignacion/_asignacion_form_success.html',
+                {'asignaciones': asignaciones}
+            )
+    else:
+        form = forms.AsignacionForm(instance=instance, prefix='asignacion')
+
+    return render(
+        request,
+        'partials/asignacion/_asignacion_form_modal.html',
+        {'form': form, 'instance': instance}
+    )
+
+
+def asignacion_eliminar(request, pk):
+    instance = get_object_or_404(Asignacion, pk=pk)
+
+    if request.method == 'POST':
+        instance.delete()
+        _, asignaciones = _filtrar_asignaciones(request)
+        return render(
+            request,
+            'partials/liquidacion/_escuela_info_result.html',
+            {'escuela': escuela, 'encargado': encargado, 'seleccionarBono': seleccionarBono}
+        )
+    return render(request, 'partials/liquidacion/_escuela_info.html', {})
+
+#Asignaciones views
+def _filtrar_asignaciones(request):
+    filtro_form = forms.FiltrarAsignacionesForm(request.GET or None)
+
+    asignaciones_list = Asignacion.objects.select_related(
+        'escuela', 'escuela__distrito', 'bono'
+    ).order_by('-fecha')
+
+    if filtro_form.is_valid():
+        distrito = filtro_form.cleaned_data.get('distrito')
+        escuela = filtro_form.cleaned_data.get('escuela')
+        bono = filtro_form.cleaned_data.get('bono')
+        fecha = filtro_form.cleaned_data.get('fecha')
+
+
+        if distrito:
+            asignaciones_list = asignaciones_list.filter(escuela__distrito=distrito)
+        if escuela:
+            asignaciones_list = asignaciones_list.filter(escuela=escuela)
+        if bono:
+            asignaciones_list = asignaciones_list.filter(bono=bono)
+        if fecha:
+            asignaciones_list = asignaciones_list.filter(fecha=fecha)
+
+    paginator = Paginator(asignaciones_list, 20)
+    asignaciones = paginator.get_page(request.GET.get('page'))
+
+    return filtro_form, asignaciones
+
+
+def home_asignaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Asignaciones', 'url': reverse('home_asignaciones')},
+    ]
+
+    filtro_form, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'asignacion/asignacionHome.html',
+        {
+            'breadcrumbs': breadcrumbs,
+            'asignaciones': asignaciones,
+            'filtro_form': filtro_form,
+            'form_media': filtro_form.media,
+        }
+    )
+
+
+def buscar_asignaciones(request):
+    _, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'partials/asignacion/_listado_asignaciones.html',
+        {'asignaciones': asignaciones}
+    )
+
+
+def asignacion_form(request, pk=None):
+    instance = get_object_or_404(Asignacion, pk=pk) if pk else None
+
+    if request.method == 'POST':
+        form = forms.AsignacionForm(request.POST, instance=instance, prefix='asignacion')
+        if form.is_valid():
+            form.save()
+            _, asignaciones = _filtrar_asignaciones(request)
+            return render(
+                request,
+                'partials/asignacion/_asignacion_form_success.html',
+                {'asignaciones': asignaciones}
+            )
+    else:
+        form = forms.AsignacionForm(instance=instance, prefix='asignacion')
+
+    return render(
+        request,
+        'partials/asignacion/_asignacion_form_modal.html',
+        {'form': form, 'instance': instance}
+    )
+
+
+def asignacion_eliminar(request, pk):
+    instance = get_object_or_404(Asignacion, pk=pk)
+
+    if request.method == 'POST':
+        instance.delete()
+        _, asignaciones = _filtrar_asignaciones(request)
+        return render(
+            request,
+            'partials/liquidacion/_escuela_info_result.html',
+            {'escuela': escuela, 'encargado': encargado, 'seleccionarBono': seleccionarBono}
+        )
+    return render(request, 'partials/liquidacion/_escuela_info.html', {})
+
+#Asignaciones views
+def _filtrar_asignaciones(request):
+    filtro_form = forms.FiltrarAsignacionesForm(request.GET or None)
+
+    asignaciones_list = Asignacion.objects.select_related(
+        'escuela', 'escuela__distrito', 'bono'
+    ).order_by('-fecha')
+
+    if filtro_form.is_valid():
+        distrito = filtro_form.cleaned_data.get('distrito')
+        escuela = filtro_form.cleaned_data.get('escuela')
+        bono = filtro_form.cleaned_data.get('bono')
+        fecha = filtro_form.cleaned_data.get('fecha')
+
+
+        if distrito:
+            asignaciones_list = asignaciones_list.filter(escuela__distrito=distrito)
+        if escuela:
+            asignaciones_list = asignaciones_list.filter(escuela=escuela)
+        if bono:
+            asignaciones_list = asignaciones_list.filter(bono=bono)
+        if fecha:
+            asignaciones_list = asignaciones_list.filter(fecha=fecha)
+
+    paginator = Paginator(asignaciones_list, 20)
+    asignaciones = paginator.get_page(request.GET.get('page'))
+
+    return filtro_form, asignaciones
+
+
+def home_asignaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Asignaciones', 'url': reverse('home_asignaciones')},
+    ]
+
+    filtro_form, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'asignacion/asignacionHome.html',
+        {
+            'breadcrumbs': breadcrumbs,
+            'asignaciones': asignaciones,
+            'filtro_form': filtro_form,
+            'form_media': filtro_form.media,
+        }
+    )
+
+
+def buscar_asignaciones(request):
+    _, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'partials/asignacion/_listado_asignaciones.html',
+        {'asignaciones': asignaciones}
+    )
+
+
+def asignacion_form(request, pk=None):
+    instance = get_object_or_404(Asignacion, pk=pk) if pk else None
+
+    if request.method == 'POST':
+        form = forms.AsignacionForm(request.POST, instance=instance, prefix='asignacion')
+        if form.is_valid():
+            form.save()
+            _, asignaciones = _filtrar_asignaciones(request)
+            return render(
+                request,
+                'partials/asignacion/_asignacion_form_success.html',
+                {'asignaciones': asignaciones}
+            )
+    else:
+        form = forms.AsignacionForm(instance=instance, prefix='asignacion')
+
+    return render(
+        request,
+        'partials/asignacion/_asignacion_form_modal.html',
+        {'form': form, 'instance': instance}
+    )
+
+
+def asignacion_eliminar(request, pk):
+    instance = get_object_or_404(Asignacion, pk=pk)
+
+    if request.method == 'POST':
+        instance.delete()
+        _, asignaciones = _filtrar_asignaciones(request)
+        return render(
+            request,
+            'partials/liquidacion/_escuela_info_result.html',
+            {'escuela': escuela, 'encargado': encargado, 'seleccionarBono': seleccionarBono}
+        )
+    return render(request, 'partials/liquidacion/_escuela_info.html', {})
+
+#Asignaciones views
+def _filtrar_asignaciones(request):
+    filtro_form = forms.FiltrarAsignacionesForm(request.GET or None)
+
+    asignaciones_list = Asignacion.objects.select_related(
+        'escuela', 'escuela__distrito', 'bono'
+    ).order_by('-fecha')
+
+    if filtro_form.is_valid():
+        distrito = filtro_form.cleaned_data.get('distrito')
+        escuela = filtro_form.cleaned_data.get('escuela')
+        bono = filtro_form.cleaned_data.get('bono')
+        fecha = filtro_form.cleaned_data.get('fecha')
+
+        if distrito:
+            asignaciones_list = asignaciones_list.filter(escuela__distrito=distrito)
+        if escuela:
+            asignaciones_list = asignaciones_list.filter(escuela=escuela)
+        if bono:
+            asignaciones_list = asignaciones_list.filter(bono=bono)
+        if fecha:
+            asignaciones_list = asignaciones_list.filter(fecha=fecha)
+
+    paginator = Paginator(asignaciones_list, 20)
+    asignaciones = paginator.get_page(request.GET.get('page'))
+
+    return filtro_form, asignaciones
+
+
+def home_asignaciones(request):
+    breadcrumbs = [
+        {'name': 'Inicio', 'url': reverse('home')},
+        {'name': 'Asignaciones', 'url': reverse('home_asignaciones')},
+    ]
+
+    filtro_form, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'asignacion/asignacionHome.html',
+        {
+            'breadcrumbs': breadcrumbs,
+            'asignaciones': asignaciones,
+            'filtro_form': filtro_form,
+            'form_media': filtro_form.media,
+        }
+    )
+
+
+def buscar_asignaciones(request):
+    _, asignaciones = _filtrar_asignaciones(request)
+
+    return render(
+        request,
+        'partials/asignacion/_listado_asignaciones.html',
+        {'asignaciones': asignaciones}
+    )
+
+
+def asignacion_form(request, pk=None):
+    instance = get_object_or_404(Asignacion, pk=pk) if pk else None
+
+    if request.method == 'POST':
+        form = forms.AsignacionForm(request.POST, instance=instance, prefix='asignacion')
+        if form.is_valid():
+            form.save()
+            _, asignaciones = _filtrar_asignaciones(request)
+            return render(
+                request,
+                'partials/asignacion/_asignacion_form_success.html',
+                {'asignaciones': asignaciones}
+            )
+    else:
+        form = forms.AsignacionForm(instance=instance, prefix='asignacion')
+
+    return render(
+        request,
+        'partials/asignacion/_asignacion_form_modal.html',
+        {'form': form, 'instance': instance}
+    )
+
+
+def asignacion_eliminar(request, pk):
+    instance = get_object_or_404(Asignacion, pk=pk)
+
+    if request.method == 'POST':
+        instance.delete()
+        _, asignaciones = _filtrar_asignaciones(request)
+        return render(
+            request,
+            'partials/liquidacion/_escuela_info_result.html',
+            {'escuela': escuela, 'encargado': encargado, 'seleccionarBono': seleccionarBono}
+        )
     return render(request, 'partials/liquidacion/_escuela_info.html', {})
 
 #Asignaciones views
