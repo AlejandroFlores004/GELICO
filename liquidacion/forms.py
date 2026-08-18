e16b1190b59f36ebd21d1f223cc37e03709a8839
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-08-18 09:06:56 -0600

    Implement filtering functionality for asignaciones with search form and pagination

--- liquidacion/forms.py
+++ liquidacion/forms.py
@@ -33,3 +33,34 @@ class SeleccionarBonoForm(forms.Form):
         super().__init__(*args, **kwargs)
         if escuela is not None:
             self.fields['bono'].queryset = Bono.objects.filter(asignacion__escuela=escuela).distinct()
+
+
+class FiltrarAsignacionesForm(forms.Form):
+    escuela = forms.ModelChoiceField(
+        queryset=Escuela.objects.all(),
+        widget=Select2Widget(attrs={
+            'data-placeholder': 'Todas las escuelas',
+            'style': 'width: 100%',
+            'class': 'select2-daisy',
+        }),
+        label='Escuela',
+        required=False,
+    )
+    bono = forms.ModelChoiceField(
+        queryset=Bono.objects.all(),
+        widget=Select2Widget(attrs={
+            'data-placeholder': 'Todos los bonos',
+            'style': 'width: 100%',
+            'class': 'select2-daisy',
+        }),
+        label='Bono',
+        required=False,
+    )
+    fecha = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={
+            'type': 'date',
+            'class': 'input input-bordered w-full',
+        }),
+        label='Fecha',
+    )
