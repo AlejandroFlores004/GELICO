fe4d89abf9bcd6ba2269c69a685aaf9bc23dd829
li21001 <li21001@ues.edu.sv>
2026-08-18 10:51:23 -0600

    Update encargadoHome template and view for consistency

--- escuela/views.py
+++ escuela/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def encargadoHomeView(request):
-    return render(request, 'encargado/encargadoHome.html')
+    return render(request, 'encargado/encargadoHome.html');
