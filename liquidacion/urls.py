c39a9e9cf3390c070b81b9d3329816e135787636
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-08-18 10:39:40 -0600

    Add asignacion form and modal for creating and editing asignaciones

--- liquidacion/urls.py
+++ liquidacion/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 
     path('asignaciones/', views.home_asignaciones, name='home_asignaciones'),
     path('asignaciones/buscar/', views.buscar_asignaciones, name='buscar_asignaciones'),
+    path('asignaciones/nueva/', views.asignacion_form, name='asignacion_nueva'),
+    path('asignaciones/<int:pk>/editar/', views.asignacion_form, name='asignacion_editar'),
 ]
