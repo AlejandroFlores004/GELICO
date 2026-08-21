22928966a0a542246bc9e7191d04c3c1bf453bd4
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-08-21 12:07:24 -0600

    Fix merge conflicts in asignaciones templates and views

--- liquidacion/urls.py
+++ liquidacion/urls.py
@@ -4,10 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_liquidaciones, name='home_liquidaciones'),
     path('escuela/buscar/', views.buscar_escuela, name='buscar_escuela'),
-
     path('asignaciones/', views.home_asignaciones, name='home_asignaciones'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('asignaciones/buscar/', views.buscar_asignaciones, name='buscar_asignaciones'),
     path('asignaciones/nueva/', views.asignacion_form, name='asignacion_nueva'),
     path('asignaciones/<int:pk>/editar/', views.asignacion_form, name='asignacion_editar'),
