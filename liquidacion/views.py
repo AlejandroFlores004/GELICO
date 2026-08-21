d82ae6c21839017fde8c77bf1c352aa90722b37f
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-08-21 12:07:24 -0600

    Fix merge conflicts in asignaciones templates and views

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -96,9 +96,6 @@ def home_asignaciones(request):
             'form_media': filtro_form.media,
         }
     )
-<<<<<<< HEAD
-=======
-
 
 def buscar_asignaciones(request):
     _, asignaciones = _filtrar_asignaciones(request)
