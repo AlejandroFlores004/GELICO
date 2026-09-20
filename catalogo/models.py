cd2c82c6d0443482a41128c0a8b8196d6ad6d516
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-09-20 10:20:16 -0600

    Add id_sistema field to Bono model and update admin configuration

--- catalogo/models.py
+++ catalogo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Bono(models.Model):
     nombre = models.CharField(max_length=255, unique=True)
     descripcion = models.TextField()
+    id_sistema = models.IntegerField(null=True, blank=True, unique=True)
 
     def __str__(self):
         return self.nombre
